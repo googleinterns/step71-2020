49d7a6b69e636194a2a1bc74aac01d75f9a85f9d
Gwyneth Chen <gwyneth.stella.chen@gmail.com>
2020-06-29 03:31:39 +0000

    Clear prior content to convert to Angular Material and add nonfunctional side navigation rail

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -1,22 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ProjectInfoComponent } from './project-info/project-info.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LeftMenuComponent } from './left-menu/left-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProjectsComponent,
-    ProjectInfoComponent
+    ProjectInfoComponent,
+    LeftMenuComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDividerModule,
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule,
+    MatTooltipModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
