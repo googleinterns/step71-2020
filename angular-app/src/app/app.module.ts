f89fb60b130a21f3e75cd9a786c7f3ddc2032a75
Gwyneth Chen <gwyneth.stella.chen@gmail.com>
2020-07-06 06:03:08 +0000

    Add HttpClientModule to the app and set up proxy.conf.js file to allow communication between front end and back end on different ports (CORS enabled)

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { MatButtonModule } from '@angular/material/button';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatDialogModule } from '@angular/material/dialog';
@@ -40,6 +41,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     AppRoutingModule,
     BrowserAnimationsModule,
     FormsModule,
+    HttpClientModule,
     MatButtonModule,
     MatChipsModule,
     MatDialogModule,
