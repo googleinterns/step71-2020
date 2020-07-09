f768f65439955897f4752b802b24bfedbfe01846
Leah <mayoleah25@gmail.com>
2020-07-09 16:22:51 +0000

    Add angular version of discovery

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DiscoveryComponent } from './discovery/discovery.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ProjectDetailComponent } from './project-detail/project-detail.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
@@ -7,6 +8,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 
 
 const routes: Routes = [
+  { path: 'discover', component: DiscoveryComponent },
   { path: 'projects/:id', component: ProjectDetailComponent },
   { path: 'projects', component: ProjectsComponent },
   { path: 'user-profile', component: UserProfileComponent },
--- angular-app/src/app/discovery/discovery.component.ts
+++ angular-app/src/app/discovery/discovery.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiscoveryComponent implements OnInit {
 
+  tagOptions: string[] = ['Producer', 'Guitar', 'HipHop', 'Singer', 'Songwriter'];
+
   constructor() { }
 
   ngOnInit(): void {
