8829ace61282a5aa93281c140aee05d49c8bd1f2
Gwyneth Chen <gwyneth.stella.chen@gmail.com>
2020-07-10 12:53:15 +0000

    Enable new project creation with a simple UI

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
 
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatChipsModule } from '@angular/material/chips';
+import { MatChipsModule, MAT_CHIPS_DEFAULT_OPTIONS, MatChipsDefaultOptions } from '@angular/material/chips';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
@@ -28,6 +30,7 @@ import { environment } from '../environments/environment';
 import { ProjectsComponent } from './projects/projects.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LeftMenuComponent } from './left-menu/left-menu.component';
+import { CreateProjectDialog } from './left-menu/left-menu.component';
 import { ChatDrawerComponent } from './chat-drawer/chat-drawer.component';
 import { ProjectDetailComponent } from './project-detail/project-detail.component';
 import { ProjectSettingsDialog } from './project-detail/project-detail.component';
@@ -46,8 +49,8 @@ import { ToggleChatService } from './toggle-chat.service';
     ProjectsComponent,
     LeftMenuComponent,
     ChatDrawerComponent,
+    CreateProjectDialog,
     ProjectDetailComponent,
-    PageNotFoundComponent,
     UserProfileComponent,
     DiscoveryComponent,
     MainFeedComponent,
@@ -69,6 +72,7 @@ import { ToggleChatService } from './toggle-chat.service';
     MatDialogModule,
     MatDividerModule,
     MatExpansionModule,
+    MatFormFieldModule,
     MatIconModule,
     MatInputModule,
     MatListModule,
@@ -81,7 +85,13 @@ import { ToggleChatService } from './toggle-chat.service';
   ],
   providers: [
     AuthService,
-    ToggleChatService
+    ToggleChatService,
+    {
+      provide: MAT_CHIPS_DEFAULT_OPTIONS,
+      useValue: {
+        separatorKeyCodes: [ ENTER, COMMA ]
+      } as MatChipsDefaultOptions
+    }
   ],
   bootstrap: [AppComponent]
 })
