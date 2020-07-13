36b758dfb3c69451fb09a0dda608fbce1a71e304
Gwyneth Chen <gwyneth.stella.chen@gmail.com>
2020-07-13 17:27:51 +0000

    Remove spamming console log

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -24,7 +24,6 @@ export class AppComponent implements OnInit, AfterViewChecked {
   }
 
   ngAfterViewChecked(): void {
-    console.log("setting chat for toggle service");
     this.toggleChatService.setChat(this.chat);
   }
 
