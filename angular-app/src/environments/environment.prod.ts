c2fa5eddd25b298993a7af1f020c82441a69ff77
Gwyneth Chen <gwyneth.stella.chen@gmail.com>
2020-07-20 21:49:47 +0000

    Copy firebase environment variables to environment.prod.ts

--- angular-app/src/environments/environment.prod.ts
+++ angular-app/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAf2uEjfbX8aE6AR_hpwUYSlEjTifrDECg",
+    authDomain: "music-collab-280719.firebaseapp.com",
+    databaseURL: "https://music-collab-280719.firebaseio.com",
+    projectId: "music-collab-280719",
+    storageBucket: "music-collab-280719.appspot.com",
+    messagingSenderId: "1051530697955",
+    appId: "1:1051530697955:web:620ab60be989799f2179d6",
+    measurementId: "G-6Q1K4XZSKD"
+  },
 };
