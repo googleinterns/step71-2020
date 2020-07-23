6d08dfbb30df7a5067c133967b0a0a764a15b42c
Gwyneth Chen <gwyneth.stella.chen@gmail.com>
2020-07-23 22:57:09 +0000

    Remove obsolete files property from Project interface

--- angular-app/src/app/project.ts
+++ angular-app/src/app/project.ts
@@ -1,10 +1,10 @@
 /**
- * Represents a single project.
+ * Represents a single project. Owner and collaborators strings must be their UIDs (user ID) 
+ * as set with Firebase authentication
  */
 export interface Project {
   title: string;
   owner: string;
   collaborators: string[];
   tags: string[];
-  files: string[];
 }
