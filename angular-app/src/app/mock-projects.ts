409819dde23bc6bad4242b5022f53e1ab9e3e3a1
Gwyneth Chen <gwyneth.stella.chen@gmail.com>
2020-07-24 00:12:11 +0000

    Remove obsolete files property from mock projects

--- angular-app/src/app/mock-projects.ts
+++ angular-app/src/app/mock-projects.ts
@@ -2,12 +2,12 @@ import { Project } from './project';
 
 export const PROJECTS: Project[] = [
   
-  { title: "Sonata 2020", owner: "Scarlatti", collaborators: ["Beethoven", "Mozart"], tags: ["classical"], files: [] },
-  { title: "A Hard Place", owner: "Keffir", collaborators: ["Baz"], tags: ["rock"], files: [] },
-  { title: "Jazz", owner: "Adam", collaborators: ["Blue", "Ronan"], tags: ["jazz", "background"], files: [] },
-  { title: "Cerulean", owner: "Anya", collaborators: ["kvitka"], tags: ["r&b", "chill"], files: [] },
-  { title: "Hip-Hop", owner: "BB", collaborators: ["Gunn"], tags: ["hip-hop"], files: [] },
-  { title: "Bubblegum", owner: "Haisha", collaborators: [], tags: ["pop", "happy"], files: [] },
-  { title: "This Nation", owner: "Yu", collaborators: ["Noh"], tags: ["country"], files: [] },
-  { title: "nitetime", owner: "zjx", collaborators: [], tags: ["edm", "dance"], files: [] },
+  { title: "Sonata 2020", owner: "Scarlatti", collaborators: ["Beethoven", "Mozart"], tags: ["classical"] },
+  { title: "A Hard Place", owner: "Keffir", collaborators: ["Baz"], tags: ["rock"] },
+  { title: "Jazz", owner: "Adam", collaborators: ["Blue", "Ronan"], tags: ["jazz", "background"] },
+  { title: "Cerulean", owner: "Anya", collaborators: ["kvitka"], tags: ["r&b", "chill"] },
+  { title: "Hip-Hop", owner: "BB", collaborators: ["Gunn"], tags: ["hip-hop"] },
+  { title: "Bubblegum", owner: "Haisha", collaborators: [], tags: ["pop", "happy"] },
+  { title: "This Nation", owner: "Yu", collaborators: ["Noh"], tags: ["country"] },
+  { title: "nitetime", owner: "zjx", collaborators: [], tags: ["edm", "dance"] },
 ];
