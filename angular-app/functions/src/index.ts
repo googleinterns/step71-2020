e12aee44e3f6fa40653924698e3036af50b917b2
Gwyneth Chen <gwyneth.stella.chen@gmail.com>
2020-08-05 02:35:15 +0000

    add Cloud function to trigger account creation

--- angular-app/functions/src/index.ts
+++ angular-app/functions/src/index.ts
@@ -1,9 +1,23 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const createProfile = functions.auth.user().onCreate(user => {
+  console.log("creating new account . . .");
+  admin.firestore().collection('users').doc(user.uid).set({
+    uid: user.uid,
+    displayName: user.displayName,
+    email: user.email,
+    photoUrl: user.photoURL,
+    bio: "",
+    tags: [],
+    following: [],
+    snippets: [],
+  }, { merge: true })
+    .then(() => console.log("New user profile created"))
+    .catch(() => console.log("Error encountered when creating new profile"));
+});
+
+export const wrfitePing = functions.firestore.document('projects/{projectId}')
+  .onWrite((change, context) => {
+    console.log("some document changed");
+});
