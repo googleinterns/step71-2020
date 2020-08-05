a0fe2c4d6f286bd79633faa1c534c142d96cf8e4
Gwyneth Chen <gwyneth.stella.chen@gmail.com>
2020-08-05 06:54:52 +0000

    delete Cloud function

--- angular-app/functions/src/index.ts
+++ angular-app/functions/src/index.ts
@@ -1,23 +1,2 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
-
-export const createProfile = functions.auth.user().onCreate(user => {
-  console.log("creating new account . . .");
-  admin.firestore().collection('users').doc(user.uid).set({
-    uid: user.uid,
-    displayName: user.displayName,
-    email: user.email,
-    photoUrl: user.photoURL,
-    bio: "",
-    tags: [],
-    following: [],
-    snippets: [],
-  }, { merge: true })
-    .then(() => console.log("New user profile created"))
-    .catch(() => console.log("Error encountered when creating new profile"));
-});
-
-export const wrfitePing = functions.firestore.document('projects/{projectId}')
-  .onWrite((change, context) => {
-    console.log("some document changed");
-});
