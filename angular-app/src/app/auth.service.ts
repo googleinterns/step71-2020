d03eab69f9061708a3ba517d9f6f1612f067a7bc
Gwyneth Chen <gwyneth.stella.chen@gmail.com>
2020-08-05 06:59:19 +0000

    Add profile creation upon login

--- angular-app/src/app/auth.service.ts
+++ angular-app/src/app/auth.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { auth } from 'firebase/app';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(public auth: AngularFireAuth) { }
+  constructor(
+    private auth: AngularFireAuth,
+    private afs: AngularFirestore,
+  ) { }
 
   getUser() {
     return this.auth.user;
@@ -18,7 +22,27 @@ export class AuthService {
     googleAuthProvider.setCustomParameters({
       prompt: 'consent' // TODO(#70): probably better as 'select_account' when in production
     });
-    this.auth.signInWithRedirect(googleAuthProvider);
+    this.auth.signInWithPopup(googleAuthProvider)
+      .then(result => {
+        let user = result.user;
+        const profileRef = this.afs.firestore.collection('users').doc(user.uid);
+        profileRef.get()
+          .then(docSnapshot => {
+            if (!docSnapshot.exists) {
+              profileRef.set({
+                uid: user.uid,
+                displayName: user.displayName,
+                email: user.email,
+                photoUrl: user.photoURL,
+                bio: "",
+                tags: [],
+                following: [],
+                snippets: [],
+              }).then(() => console.log("New user profile created"))
+                .catch(() => console.log("Error encountered when creating new profile"));
+            }
+          });
+      });
   }
 
   logout() {
