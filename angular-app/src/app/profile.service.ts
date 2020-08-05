5710de4556c57459a1a5dfd8a5c166919605f308
Gwyneth Chen <gwyneth.stella.chen@gmail.com>
2020-08-05 07:28:59 +0000

    move uaser/auth logic from profile service to client user-profile component to avoid async issues

--- angular-app/src/app/profile.service.ts
+++ angular-app/src/app/profile.service.ts
@@ -3,34 +3,19 @@ import { Observable, Subject } from 'rxjs';
 import * as firebase from 'firebase/app'
 import { AngularFirestore } from '@angular/fire/firestore';
 
-import { AuthService } from './auth.service';
 import { Profile } from './profile';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
-  private user: firebase.User;
-  private currentProfile$: Observable<Profile>;
 
   constructor(
     private firestore: AngularFirestore,
-    private authService: AuthService,
-  ) {
-    this.authService.getUser().subscribe(user => {
-      this.user = user;
-      if (this.user != null) {
-        this.currentProfile$ = this.getProfile(this.user.uid);
-      }
-    });
-  }
+  ) { }
 
   public getProfile(uid: string): Observable<Profile> {
     return this.firestore.collection('users').doc<Profile>(uid).valueChanges();
-  }
-
-  public getCurrentProfile(): Observable<Profile> {
-    return this.currentProfile$;
   }
 
   public saveProfile() {
--- angular-app/src/app/user-profile/user-profile.component.ts
+++ angular-app/src/app/user-profile/user-profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 
+import { AuthService } from '../auth.service';
 import { ProfileService } from '../profile.service';
 import { Profile } from '../profile';
 
@@ -10,15 +11,17 @@ import { Profile } from '../profile';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
-
   profile$: Observable<Profile>;
   
   constructor(
+    private authService: AuthService,
     private profileService: ProfileService,
   ) { }
 
   ngOnInit() {
-    this.profile$ = this.profileService.getCurrentProfile();
+    this.authService.getUser().subscribe(user => {
+      this.profile$ = this.profileService.getProfile(user.uid);
+    });
   }
 
 }
